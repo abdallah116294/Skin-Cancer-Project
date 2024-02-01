5058d0cc94a46996ab66e06b4bb058bfec81885a
abdallah <abdallhamohamed116@gmail.com>
2024-02-01 22:26:17 +0200

    chore:Update onboarding

--- mobile/lib/features/onBoarding/on_boarding_screen.dart
+++ mobile/lib/features/onBoarding/on_boarding_screen.dart
@@ -10,43 +10,91 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-       decoration: BoxDecoration(
-          gradient: LinearGradient(colors: AppColor.onBoardingColor,begin: Alignment.centerRight,end: Alignment.centerLeft)
-        ),
-      child: const Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage(AssetsManager.onBoarding),width: 373,height: 415,),
-            SizedBox(
-              height: 7,
+    return  Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+            bottom: 1,
+            child: ClipPath(
+              clipper: CustomClipPath3(),
+              child: Container(
+                width: 468.43,
+                height: 419.26,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: AppColor.onBoardingColor)),
+              ),
+            )),
+        Positioned(
+            top: 1,
+            right: 1,
+            child: ClipPath(
+              clipper: CustomClipPath2(),
+              child: Container(
+                width: 338,
+                height: 180,
+                decoration: const BoxDecoration(color: AppColor.onBoarding2),
+              ),
+            )),
+        CustomPaint(
+          painter: RPSCustomPainter(),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: AppColor.onBoardingColor,)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage(AssetsManager.onBoarding),
+                  width: 373,
+                  height: 415,
+                ),
+                const Text(
+                  StringManager.onboarding1,
+                  style: TextStyle(
+                      color: AppColor.textonBoardingColo,
+                      overflow: TextOverflow.clip,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  StringManager.onBoarding2,
+                  style: TextStyle(
+                      color: AppColor.textonBoardingColo,
+                      overflow: TextOverflow.clip,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, Routes.choseUserRoutes);
+                  },
+                  child: Container(
+                    width: 358,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(colors: AppColor.btnGradient)),
+                    child: const Center(
+                      child: Text(
+                        StringManager.getStarted,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-            Text(
-              StringManager.onboarding1,
-              style: TextStyle(
-                  color: AppColor.textonBoardingColo,
-                  overflow: TextOverflow.clip,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20),
-            ),
-               SizedBox(
-              height: 7,
-            ),
-            Text(
-              StringManager.onBoarding2,
-              style: TextStyle(
-                  color: AppColor.textonBoardingColo,
-                  overflow: TextOverflow.clip,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14),
-            )
-            
-          ],
-        ),
-      ),
-    );
+          ),
+        )
+      ],
+    ));
   }
 }
