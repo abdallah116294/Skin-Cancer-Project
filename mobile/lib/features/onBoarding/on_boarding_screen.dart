c664f055a5b436bf9c7b0f044f368c4d2672c3d5
abdallah <abdallhamohamed116@gmail.com>
2024-02-01 13:59:28 +0200

    chore:line🤷‍♂️

--- mobile/lib/features/onBoarding/on_boarding_screen.dart
+++ mobile/lib/features/onBoarding/on_boarding_screen.dart
@@ -8,43 +8,37 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-       decoration: BoxDecoration(
-          gradient: LinearGradient(colors: AppColor.onBoardingColor,begin: Alignment.centerRight,end: Alignment.centerLeft)
-        ),
-      child: const Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage(AssetsManager.onBoarding),width: 373,height: 415,),
-            SizedBox(
-              height: 7,
-            ),
-            Text(
-              StringManager.onboarding1,
-              style: TextStyle(
-                  color: AppColor.textonBoardingColo,
-                  overflow: TextOverflow.clip,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20),
-            ),
-               SizedBox(
-              height: 7,
-            ),
-            Text(
-              StringManager.onBoarding2,
-              style: TextStyle(
-                  color: AppColor.textonBoardingColo,
-                  overflow: TextOverflow.clip,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14),
-            )
-            
-          ],
-        ),
-      ),
-    );
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: AppColor.onBoardingColor,
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft)),
+        child: const Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Image(
+                image: AssetImage(AssetsManager.onBoarding),
+                width: 373,
+                height: 415,
+              ),
+              SizedBox(
+                height: 7,
+              ),
+              Text(
+                StringManager.onboarding1,
+                style: TextStyle(
+                    color: AppColor.textonBoardingColo,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20),
+              )
+            ],
+          ),
+        ));
   }
 }
