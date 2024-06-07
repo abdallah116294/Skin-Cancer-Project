a7b876af13cc97e1cb269d4ba12f8a747e0ae077
abdallah <abdallhamohamed116@gmail.com>
2024-06-07 19:10:14 +0300

    updating profile screen logic

--- mobile/lib/features/profile/widgets/hall_screen_widget.dart
+++ mobile/lib/features/profile/widgets/hall_screen_widget.dart
@@ -5,6 +5,7 @@ import 'package:mobile/core/helper/exetentions.dart';
 import 'package:mobile/core/helper/spacing.dart';
 import 'package:mobile/core/utils/app_color.dart';
 import 'package:mobile/core/utils/text_styles.dart';
+import 'package:mobile/features/appointment/screen/appointment_screen.dart';
 import 'package:mobile/features/payments/screens/register_to_payment.dart';
 import 'package:mobile/features/profile/widgets/profile_widget.dart';
 
@@ -65,50 +66,26 @@ class HallProfileScreenWidget extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 8.w),
             child: Column(
               children: [
-                // dotoctorRole != null
-                //     ? ProfileWidget(
-                //         leading: const Icon(
-                //             color: Color(0xFF6671EB), Icons.calendar_month),
-                //         title: 'Appointments',
-                //         onTap: () {
-                //           Navigator.of(context).push(MaterialPageRoute(
-                //               builder: (context) => const AppointmentScreen(
-                //                   //    uid: uid,
-                //                   )));
-                //         },
-                //       )
-                //     :
-                ProfileWidget(
-                  backgroundColor: const Color(0xFFE9FAEF),
-                  iconPath: "assets/svgs/print.svg",
-                  title: 'My Test & Diagnostic',
-                  onTap: () {
-                    context.pushNamed(Routes.aIHistoryScreen);
-                  },
-                ),
-
-                // ProfileWidget(
-                //   leading: Icon(color: Color(0xFF6671EB), Icons.favorite),
-                //   title: 'Favorites',
-                // ),
-                // dotoctorRole == null?
-                //     :
-                // ProfileWidget(
-                //         onTap: () {
-                //           context
-                //               .pushNamed(Routes.docClinicDetailsScreenRoutes);
-                //           // Navigator.push(
-                //           //     context,
-                //           //     MaterialPageRoute(
-                //           //         builder: (context) => ClinicDoctorDetials(
-                //           //               uid: uid,
-                //           //             )));
-                //         },
-                //         leading: const Icon(
-                //             color: Color(0xFF6671EB),
-                //             Icons.local_hospital_rounded),
-                //         title: 'My Clinic',
-                //       ),
+                dotoctorRole != null
+                    ? ProfileWidget(
+                        backgroundColor: AppColor.primaryColor.withOpacity(.5),
+                        iconPath: "assets/svgs/clinic_book.svg",
+                        title: 'Appointments',
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const AppointmentScreen(
+                                  //    uid: uid,
+                                  )));
+                        },
+                      )
+                    : ProfileWidget(
+                        backgroundColor: const Color(0xFFE9FAEF),
+                        iconPath: "assets/svgs/print.svg",
+                        title: 'My Test & Diagnostic',
+                        onTap: () {
+                          context.pushNamed(Routes.aIHistoryScreen);
+                        },
+                      ),
                 ProfileWidget(
                   title: 'Payment Method',
                   onTap: () {
@@ -120,15 +97,32 @@ class HallProfileScreenWidget extends StatelessWidget {
                   iconPath: 'assets/svgs/payment.svg',
                   backgroundColor: Color(0xFFFFEEEF),
                 ),
+                dotoctorRole != null
+                    ? ProfileWidget(
+                        onTap: () {
+                          context
+                              .pushNamed(Routes.docClinicDetailsScreenRoutes);
+                        },
+                        title: 'Clinic',
+                        iconPath: 'assets/svgs/clinic_book.svg',
+                        backgroundColor: AppColor.primaryColor.withOpacity(.5),
+                      )
+                    : ProfileWidget(
+                        onTap: () {
+                          context.pushNamed(Routes.patientSelectedClinic);
+                        },
+                        title: 'Booked Clinic',
+                        iconPath: 'assets/svgs/clinic_book.svg',
+                        backgroundColor: AppColor.primaryColor.withOpacity(.5),
+                      ),
                 ProfileWidget(
+                  iconPath: "assets/svgs/developer.svg",
+                  backgroundColor: Color(0xf0088ff),
                   onTap: () {
-                    context.pushNamed(Routes.patientSelectedClinic);
+                    context.pushNamed(Routes.developerScreen);
                   },
-                  title: 'Booked Clinic',
-                  iconPath: 'assets/svgs/clinic_book.svg',
-                  backgroundColor: AppColor.primaryColor.withOpacity(.5),
+                  title: 'Developer',
                 ),
-
                 ProfileWidget(
                   title: 'Log out',
                   onTap: () {
@@ -150,38 +144,6 @@ class HallProfileScreenWidget extends StatelessWidget {
                   iconPath: 'assets/svgs/logout.svg',
                   backgroundColor: const Color(0xFFFFEEEF),
                 ),
-                // ProfileWidget(
-                //   onTap: () {
-                //     context.pushNamed(Routes.developerScreen);
-                //     // Navigator.push(
-                //     //     context, MaterialPageRoute(builder: (context)=>const DeveloperScreen()));
-                //   },
-                //   leading: const Icon(
-                //       color: Color(0xFF6671EB), Icons.developer_mode),
-                //   title: 'Developer',
-                // ),
-                // ProfileWidget(
-                //   onTap: () {
-                //     DailogAlertFun.showMyDialog(
-                //         daliogContent: "Are you sure?",
-                //         actionName: "Logout",
-                //         context: context,
-                //         onTap: () {
-                //           CacheHelper.removeData(key: 'token');
-                //           CacheHelper.removeData(key: 'email');
-                //           if (dotoctorRole != null) {
-                //             CacheHelper.removeData(key: 'doctor_role');
-                //           }
-                //           context.pushNamedAndRemoveUntil(
-                //               Routes.choseUserRoutes,
-                //               predicate: (Route<dynamic> route) => false);
-                //         });
-                //   },
-                //   leading: const Icon(
-                //       color: Color(0xFF6671EB), Icons.lock_outline_rounded),
-                //   title: 'Log out',
-                //   iconPath: '',
-                // ),
               ],
             ),
           ),
